c027084f425439e2ea5071481f63e90f6389a640
3rik17 <pinaresjohn@gmail.com>
2017-11-21 22:59:16 -0500

    Matavichos Version 5.0
    Simple version para sistemas windows xp/7

---
+++ reset_desktop.bat
@@ -0,0 +1,12 @@
+@echo off
+@break off
+@title MATA VICHOS V.5 por [[ John Erik R. ]] 
+@color 0a
+@cls
+@mode con cols=55 lines=1
+
+PAUSE
+taskkill /im explorer.exe /f
+start explorer.exe
+taskkill /IM cmd.exe
+
